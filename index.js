61a84603490414951ab0b534888aee74fc555f2f
Kamyar Taghavi <caracicatriz.kt@gmail.com>
2023-05-19 23:08:27 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat (name) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name)
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [...cats];
+    newCats.unshift(name)
+    return newCats;
+}
+
+function removeLastCat(name) {
+    const newCats = [...cats];
+    newCats.pop(name)
+    return newCats;
+}
+
+function removeFirstCat(name) {
+    const newCats = [...cats];
+    newCats.shift(name)
+    return newCats;
+}
